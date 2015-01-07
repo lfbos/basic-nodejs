d1011e4dc0d94c6cdc5891b104c6c8a236285fde
Luis Boscan <luife642@gmail.com>
2015-01-07 18:55:46 -0430

    Sirviendo páginas HTML creando un servidor web HTML

---
+++ 5/servidor.js
@@ -0,0 +1,26 @@
+var servidor = require('http');
+var url 		 = require('url');
+var fs 		 = require('fs');
+
+function iniciar(enrutar, manejador) {
+	function arrancaServidor(requiere, respuesta) {
+
+		var ruta = url.parse(requiere.url).pathname;
+		
+		console.log("Alguien se ha conectado");
+
+		// var contenido = enrutar(manejador, ruta, respuesta);
+
+		var index = fs.readFileSync("www/index.html");
+
+		var registro = fs.createWriteStream('registro.txt', {'flags': 'a'});
+		registro.write(ruta + '\n');
+		
+		respuesta.writeHead(200, {"Content-Type": "text/html"});
+		respuesta.write(index);
+		respuesta.end();
+	}
+	servidor.createServer(arrancaServidor).listen(8888);
+}
+
+exports.iniciar = iniciar;
